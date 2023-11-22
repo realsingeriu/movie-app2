621a0dc74b0c05d74d3bf7f98075a4305e7a213b
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-22 09:37:07 +0900

    App정리, CSS정리  및 폰트적용과 Navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/MovieList/Navbar/Navbar'
+
+export default function App() {
+  return (
+    <div className='app'>
+      <Navbar />
+
+      <main>Main Content</main>
+    </div>
+  );
+}
+
